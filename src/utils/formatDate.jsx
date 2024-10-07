34d3491e14666417d5ba56f63ef29532a8162fb8
Jahnavi Garikapati <jahnavig310@gmail.com>
2024-10-08 00:28:20 +0530

    Fix: Time to AM/PM

--- src/utils/formatDate.jsx
+++ src/utils/formatDate.jsx
@@ -1,7 +1,11 @@
 export const formatDate = (date) => {
-    const options = { day: 'numeric', month: 'long', year: 'numeric' };
-    const formattedDate = new Date(date).toLocaleDateString('en-GB', options);
-    const time = new Date(date).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' });
-    return `${formattedDate} • ${time}`;
-  };
-  
+  const options = { day: 'numeric', month: 'long', year: 'numeric' };
+  const formattedDate = new Date(date).toLocaleDateString('en-GB', options);
+    const time = new Date(date).toLocaleTimeString('en-GB', { 
+    hour: '2-digit', 
+    minute: '2-digit',
+    hour12: true
+  }).replace('am', 'AM').replace('pm', 'PM');;
+
+  return `${formattedDate} • ${time}`;
+};
